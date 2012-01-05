04fe118e1619a3a09640303359eb89da4e6f661c
JT Archie <jtarchie@gmail.com>
2012-01-04 23:44:55 -0500

    release cycle for the language pack

---
+++ lib/language_pack.rb
@@ -0,0 +1,20 @@
+require "pathname"
+
+# General Language Pack module
+module LanguagePack
+
+  # detects which language pack to use
+  # @param [Array] first argument is a String of the build directory
+  # @return [LanguagePack] the {LanguagePack} detected
+  def self.detect(*args)
+    Dir.chdir(args.first)
+
+    pack = [ Nginx ].detect do |klass|
+      klass.use?
+    end
+
+    pack ? pack.new(*args) : nil
+  end
+end
+
+require 'language_pack/nginx'
---
+++ lib/language_pack/nginx.rb
@@ -0,0 +1,46 @@
+require "tmpdir"
+require "rubygems"
+require "language_pack"
+require "language_pack/base"
+
+class LanguagePack::Nginx < LanguagePack::Base
+  OPENRESTY_STABLE_VERSION = "1.0.10.24"
+
+  def self.use?
+    File.exist?("nginx.conf")
+  end
+  
+  def name
+    "Nginx"
+  end
+
+  def default_addons
+    ['shared-database:5mb']
+  end
+
+  def default_config_vars
+    {
+      "LANG"     => "en_US.UTF-8",
+      "PATH"     => default_path
+    }
+  end
+
+  def default_process_types
+    {
+      "web" => "nginx -c nginx.conf -g \"$port_number = ${PORT}\""
+    }
+  end
+
+
+  def compile
+    Dir.chdir(build_path)
+    run("curl #{VENDOR_URL}/openresty_nginx-#{OPENRESTY_STABLE_VERSION}.tar.gz -s -o - | tar zxf -")
+    Dir["nginx/nginx/sbin/*"].each {|path| run("chmod +x #{path}") }
+    File.mkdir_p "logs"
+  end
+
+  private 
+  def default_path
+    "bin:/bin:/usr/local/bin:/usr/bin:/bin:/app/nginx/nginx/sbin"
+  end
+end
