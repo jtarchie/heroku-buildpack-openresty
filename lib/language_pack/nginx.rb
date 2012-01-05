47d90734b0f7c2b79130c72fa65063ff2e632bde
JT Archie <jtarchie@gmail.com>
2012-01-05 01:48:34 -0500

    with respect to the current directory

--- lib/language_pack/nginx.rb
+++ lib/language_pack/nginx.rb
@@ -27,7 +27,7 @@ class LanguagePack::Nginx < LanguagePack::Base
 
   def default_process_types
     {
-      "web" => 'sed -i "s/\$port/${PORT}/" nginx.conf && nginx -c nginx.conf;'
+      "web" => 'sed -i "s/\$port/${PORT}/" `pwd`/nginx.conf && nginx -c `pwd`/nginx.conf;'
     }
   end
 
