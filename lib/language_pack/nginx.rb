cb7be688a0761c22bb34c1422681f7b8766e80a4
JT Archie <jtarchie@gmail.com>
2012-01-05 02:00:51 -0500

    update sed syntax

--- lib/language_pack/nginx.rb
+++ lib/language_pack/nginx.rb
@@ -27,7 +27,7 @@ class LanguagePack::Nginx < LanguagePack::Base
 
   def default_process_types
     {
-      "web" => 'sed -i "s/\$port/${PORT}/" `pwd`/nginx.conf && nginx -c `pwd`/nginx.conf;'
+      "web" => 'sed "s/\$port/${PORT}/" `pwd`/nginx.conf && nginx -c `pwd`/nginx.conf;'
     }
   end
 
