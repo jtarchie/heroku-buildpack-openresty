3a88f6267140e2765d4907795a3b8efd64ad19c3
JT Archie <jtarchie@gmail.com>
2012-09-05 00:54:15 -0400

    extract DATABASE_URL into other ENV vars

--- lib/language_pack/scripts/runner.rb
+++ lib/language_pack/scripts/runner.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/env ruby
 
+if ENV['DATABASE_URL']
+  database_url = URI.parse(ENV['DATABASE_URL'])
+  ENV['DB_HOST'] = database_url.host
+  ENV['DB_NAME'] = database_url.path.sub(/^\//,'')
+  ENV['DB_USERNAME'] = database_url.user
+  ENV['DB_PASSWORD'] = database_url.password
+end
+
 modified_filename = "nginx-#{Time.now.to_i}.conf"
 conf_file = File.read("nginx.conf").gsub!(/\$ENV_(\w+)/) { ENV[$1] }
 File.open(modified_filename, "w") {|f| f.puts conf_file }
