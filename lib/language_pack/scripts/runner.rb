816ff97aabc99c5f44f16a4d58266b64f639acda
JT Archie <jtarchie@gmail.com>
2012-09-30 20:06:00 -0400

    support for echoing out error and access log

--- lib/language_pack/scripts/runner.rb
+++ lib/language_pack/scripts/runner.rb
@@ -13,4 +13,4 @@ end
 modified_filename = "nginx-#{Time.now.to_i}.conf"
 conf_file = File.read("nginx.conf").gsub!(/\$ENV_(\w+)/) { ENV[$1] }
 File.open(modified_filename, "w") {|f| f.puts conf_file }
-%x{nginx -c \`pwd\`/#{modified_filename} -g "daemon off;"}
+%x{touch logs/access.log logs/error.log && (tail -f -n 0 logs/*.log &) && nginx -c \`pwd\`/#{modified_filename} -g "daemon off;"}
